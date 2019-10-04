fb164b2c5451a3ff3b86d8cee72d1faae071fb63
Jason IBISI <jasonibisi@gmail.com>
2019-10-04 18:50:00 +0100

    Added Logout Service and refactored Auth Service

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import jwtDecode from 'jwt-decode';
 import NavBar from './components/navBar';
 import Movies from './components/movies';
 import Customers from './components/customers';
@@ -9,7 +8,9 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
+import Logout from './components/logout';
 import RegisterForm from './components/registerForm';
+import auth from './services/authService';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
@@ -17,11 +18,8 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    try {
-      const jwt = localStorage.getItem('token');
-      const user = jwtDecode(jwt);
-      this.setState({ user });
-    } catch (ex) {}
+    const user = auth.getCurrentUser();
+    this.setState({ user });
   }
 
   render() {
@@ -32,6 +30,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies/new" component={MovieForm} />
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
-import { login } from '../services/authService';
+import auth from '../services/authService';
 
 export class LoginForm extends Form {
   state = { data: { username: '', password: '' }, errors: {} };
@@ -18,8 +18,7 @@ export class LoginForm extends Form {
   doSubmit = async () => {
     try {
       const { data } = this.state;
-      const { data: jwt } = await login(data.username, data.password);
-      localStorage.setItem('token', jwt);
+      await auth.login(data.username, data.password);
       window.location = '/';
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
